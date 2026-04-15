2c35301a6514207e055032f0daa13a4f730455ed
Zakharov Svyatoslav <kraldraav@gmail.com>
2026-04-15 23:44:39 +0300

    move_semantic3 complete

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,8 +1,8 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
-    vec
+    vec.to_vec()
 }
 
 fn main() {
