b394e7de79b336f113433d719909f676d4bea3dc
Zakharov Svyatoslav <kraldraav@gmail.com>
2026-04-27 22:15:22 +0300

    try_from_into complete

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -23,19 +23,39 @@ enum IntoColorError {
     IntConversion,
 }
 
+fn is_valid_int(i: i16) -> bool {
+    (0..=255).contains(&i)
+}
+
+fn convert_to_u8(i: i16) -> u8 {
+    i as u8
+}
+
 // TODO: Tuple implementation.
 // Correct RGB color values must be integers in the 0..=255 range.
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if !is_valid_int(tuple.0) || !is_valid_int(tuple.1) || !is_valid_int(tuple.2){
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Color { red: convert_to_u8(tuple.0), green: convert_to_u8(tuple.1), blue: convert_to_u8(tuple.2) })
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if !is_valid_int(arr[0]) || !is_valid_int(arr[1]) || !is_valid_int(arr[2]){
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Color { red: convert_to_u8(arr[0]), green: convert_to_u8(arr[1]), blue: convert_to_u8(arr[2]) })
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +63,19 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        for &value in slice {
+            if !is_valid_int(value) {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+
+        Ok(Color { red: convert_to_u8(*slice.first().unwrap()), green: convert_to_u8(*slice.get(1).unwrap()), blue: convert_to_u8(*slice.get(2).unwrap()) })
+    }
 }
 
 fn main() {
